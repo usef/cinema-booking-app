64d5bdc017ffb67a1e64f36582f045bb1aa2341b
ahmedAshref <58373471+ahmedAshrefsw@users.noreply.github.com>
2021-02-02 01:50:45 +0200

    building addmovie screen - without image

--- vendor_app/lib/screens/AddMovieScreen.dart
+++ vendor_app/lib/screens/AddMovieScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vendor_app/Palette.dart';
+import 'package:intl/intl.dart';
 
 class AddMovieScreen extends StatefulWidget {
   @override
@@ -7,20 +8,135 @@ class AddMovieScreen extends StatefulWidget {
 }
 
 class _AddMovieScreenState extends State<AddMovieScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  DateTime _datePicked = DateTime.now();
+  TimeOfDay _timePicked = TimeOfDay.now();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Material(
-        color: Palette.mainColor,
-        child: Center(
-          child: IconButton(
-            icon: Icon(Icons.arrow_back),
-            color: Palette.secondaryColor,
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(" add movie "),
+        centerTitle: true,
       ),
-    );  }
+      body: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              Padding(
+                  padding: const EdgeInsets.all(50),
+                  child: TextFormField(
+                    decoration:
+                        const InputDecoration(hintText: 'enter the movie name'),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "please enter a name";
+                      }
+                      return null;
+                    },
+                  )),
+              Padding(
+                  padding: const EdgeInsets.only(left: 50, right: 50),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                        hintText: 'enter the movie description'),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "please enter movie description";
+                      }
+                      return null;
+                    },
+                  )),
+              SizedBox(
+                height: 20,
+              ),
+              // the next row contains pick date button and the date display
+              Row(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(left: 15),
+                    child: ElevatedButton(
+                      child: Text("pick a date"),
+                      onPressed: () {
+                        showDatePicker(
+                                context: context,
+                                initialDate: DateTime.now(),
+                                firstDate: DateTime(2020),
+                                lastDate: DateTime(2022))
+                            .then((datePicked) {
+                          setState(() {
+                            if (datePicked != null) {
+                              _datePicked = datePicked;
+                            }
+                          });
+                        });
+                      },
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(left: 10),
+                    child: Text(new DateFormat.yMMMd().format(_datePicked)),
+                  ),
+                ],
+              ),
+              // the next row contains pick time button and the time display
+              Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(left: 15),
+                    child: ElevatedButton(
+                      child: Text("pick a TIME"),
+                      onPressed: () {
+                        showTimePicker(
+                                context: context, initialTime: TimeOfDay.now())
+                            .then((timePicked) {
+                          setState(() {
+                            if (timePicked != null) {
+                              _timePicked = timePicked;
+                            }
+                          });
+                        });
+                      },
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(left: 10),
+                    child: Text(_timePicked.format(context)),
+                  ),
+                ],
+              ),
+              // the next widget contains submit button and action
+              Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Validate will return true if the form is valid, or false if
+                      // the form is invalid.
+                      if (_formKey.currentState.validate()) {
+                        // Process data.
+                      }
+                    },
+                    child: Text('Submit'),
+                  )),
+            ],
+          )),
+    );
+  }
 }
+
+//usef code
+// Container(
+//       child: Material(
+//         color: Palette.mainColor,
+//         child: Center(
+//           child: IconButton(
+//             icon: Icon(Icons.arrow_back),
+//             color: Palette.secondaryColor,
+//             onPressed: () {
+//               Navigator.pop(context);
+//             },
+//           ),
+//         ),
+//       ),
+//     );
