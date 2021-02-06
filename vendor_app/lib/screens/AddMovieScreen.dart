307043f85e5cf8b17856bbad01a3bcbe4b4f3630
ahmedAshref <58373471+ahmedAshrefsw@users.noreply.github.com>
2021-02-06 10:56:23 +0200

    expanded added

--- vendor_app/lib/screens/AddMovieScreen.dart
+++ vendor_app/lib/screens/AddMovieScreen.dart
@@ -25,124 +25,127 @@ class _AddMovieScreenState extends State<AddMovieScreen> {
         title: Text(" add movie "),
         centerTitle: true,
       ),
-      body: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              Padding(
-                  padding: const EdgeInsets.all(50),
-                  child: TextFormField(
-                    decoration:
-                        const InputDecoration(hintText: 'enter the movie name'),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return "please enter a name";
-                      }
-                      return null;
-                    },
-                  )),
-              Padding(
-                  padding: const EdgeInsets.only(left: 50, right: 50),
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                        hintText: 'enter the movie description'),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return "please enter movie description";
-                      }
-                      return null;
-                    },
-                  )),
-              SizedBox(
-                height: 20,
-              ),
-              // the next row contains pick date button and the date display
-              Row(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.only(left: 15),
-                    child: ElevatedButton(
-                      child: Text("pick a date"),
-                      onPressed: () {
-                        showDatePicker(
-                                context: context,
-                                initialDate: DateTime.now(),
-                                firstDate: DateTime(2020),
-                                lastDate: DateTime(2022))
-                            .then((datePicked) {
-                          setState(() {
-                            if (datePicked != null) {
-                              _datePicked = datePicked;
-                            }
-                          });
-                        });
+      body: Expanded(
+        child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                Padding(
+                    padding: const EdgeInsets.all(50),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                          hintText: 'enter the movie name'),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return "please enter a name";
+                        }
+                        return null;
                       },
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(left: 10),
-                    child: Text(new DateFormat.yMMMd().format(_datePicked)),
-                  ),
-                ],
-              ),
-              // the next row contains pick time button and the time display
-              Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(left: 15),
-                    child: ElevatedButton(
-                      child: Text("pick a TIME"),
-                      onPressed: () {
-                        showTimePicker(
-                                context: context, initialTime: TimeOfDay.now())
-                            .then((timePicked) {
-                          setState(() {
-                            if (timePicked != null) {
-                              _timePicked = timePicked;
-                            }
-                          });
-                        });
+                    )),
+                Padding(
+                    padding: const EdgeInsets.only(left: 50, right: 50),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                          hintText: 'enter the movie description'),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return "please enter movie description";
+                        }
+                        return null;
                       },
+                    )),
+                SizedBox(
+                  height: 20,
+                ),
+                // the next row contains pick date button and the date display
+                Row(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.only(left: 15),
+                      child: ElevatedButton(
+                        child: Text("pick a date"),
+                        onPressed: () {
+                          showDatePicker(
+                                  context: context,
+                                  initialDate: DateTime.now(),
+                                  firstDate: DateTime(2020),
+                                  lastDate: DateTime(2022))
+                              .then((datePicked) {
+                            setState(() {
+                              if (datePicked != null) {
+                                _datePicked = datePicked;
+                              }
+                            });
+                          });
+                        },
+                      ),
                     ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(left: 10),
-                    child: Text(_timePicked.format(context)),
-                  ),
-                ],
-              ),
-              Container(
-                  height: 200,
-                  width: 200,
-                  child: _image == null
-                      ? Text(
-                          "no image selected",
-                          style: TextStyle(fontSize: 20),
-                        )
-                      : Image.file(_image)),
-              ElevatedButton(
-                child: Text("camera"),
-                onPressed: getImageByCamera,
-              ),
-              ElevatedButton(
-                child: Text("gallery"),
-                onPressed: getImageByGallery,
-              ),
-              // the next widget contains submit button and action
-              Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Validate will return true if the form is valid, or false if
-                      // the form is invalid.
-                      if (_formKey.currentState.validate()) {
-                        // Process data.
-                      }
-                    },
-                    child: Text('Submit'),
-                  )),
-            ],
-          )),
+                    Container(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Text(new DateFormat.yMMMd().format(_datePicked)),
+                    ),
+                  ],
+                ),
+                // the next row contains pick time button and the time display
+                Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(left: 15),
+                      child: ElevatedButton(
+                        child: Text("pick a TIME"),
+                        onPressed: () {
+                          showTimePicker(
+                                  context: context,
+                                  initialTime: TimeOfDay.now())
+                              .then((timePicked) {
+                            setState(() {
+                              if (timePicked != null) {
+                                _timePicked = timePicked;
+                              }
+                            });
+                          });
+                        },
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Text(_timePicked.format(context)),
+                    ),
+                  ],
+                ),
+                Container(
+                    height: 200,
+                    width: 200,
+                    child: _image == null
+                        ? Text(
+                            "no image selected",
+                            style: TextStyle(fontSize: 20),
+                          )
+                        : Image.file(_image)),
+                ElevatedButton(
+                  child: Text("camera"),
+                  onPressed: getImageByCamera,
+                ),
+                ElevatedButton(
+                  child: Text("gallery"),
+                  onPressed: getImageByGallery,
+                ),
+                // the next widget contains submit button and action
+                Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Validate will return true if the form is valid, or false if
+                        // the form is invalid.
+                        if (_formKey.currentState.validate()) {
+                          // Process data.
+                        }
+                      },
+                      child: Text('Submit'),
+                    )),
+              ],
+            )),
+      ),
     );
   }
 
